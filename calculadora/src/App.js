e21adcda25953f2c41426ac6167e4f01d48f0474
Ana Laura <analaurass45@gmail.com>
2023-07-18 16:25:52 -0300

    first commit

---
+++ calculadora/src/App.js
@@ -0,0 +1,11 @@
+import Calculadora from './components/calculadora/Calculadora';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora></Calculadora>  
+    </div>
+  );
+}
+
+export default App;
